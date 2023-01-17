c4b4d432639bcf1d34ca0bc9f1f688ce3fb703e3
Suhendri <aretekings@gmail.com>
2023-01-17 20:41:38 +0700

    UAS202102257
    UAS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MahasiswaComponent,
   ],
   imports: [
     BrowserModule,
